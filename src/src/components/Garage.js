8a21149c3217d3cc338d7ef88b09d7a284ba5b6d
AlexTan21 <87435891+AlexTan21@users.noreply.github.com>
2021-11-19 17:36:20 -0500

    Add files via upload

---
+++ src/src/components/Garage.js
@@ -0,0 +1,18 @@
+const Garage = (props) => {
+    const { img, name, price, description, id } = props.product;
+  
+    return (
+      <div className="products" id={id}>
+        <h3>{name}</h3>
+        <div>Price: {"$" + price.toFixed(2)} </div>
+        <div>
+          <img src={img} alt={name} />
+        </div>
+        <div>{description}</div>
+        <button type="submit" onClick={() => props.handleProductClick(props.product)}>
+          {" "}
+          Add To Cart
+        </button>
+      </div>
+    );
+  };
